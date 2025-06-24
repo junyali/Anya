75967961b05b75f18c5e8f0cfe973653863d9747
Junya <junjia.liu@pm.me>
2025-06-24 04:58:21 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# super secret stuff z0mg11!!
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print("hello i am in fact alive :3")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    is_in_dm = isinstance(message.channel, discord.DMChannel)
+    is_mentioned = bot.user in message.mentions
+    is_replied = message.reference and message.reference.resolved and message.reference.resolved.author == bot.user
+
+    if (is_in_dm and is_mentioned and is_replied):
+        # uhh idk make the bot respond or smthin
+        pass
+
+bot.run(TOKEN)
