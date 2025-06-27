033d534313c7d04b8ecfc9774249204435a3db2a
Junya <junjia.liu@pm.me>
2025-06-27 01:39:09 +0100

    Basic checks so people don't start abusing this :p

--- moderation_handler.py
+++ moderation_handler.py
@@ -1,3 +1,4 @@
+import discord
 from typing import Optional, Dict, Any, List
 from enum import Enum
 from dataclasses import dataclass
@@ -40,3 +41,40 @@ class ModerationParse:
 		message_lower = message.lower()
 		return any(keyword in message_lower for keyword in cls.MODERATION_KEYWORDS.keys())
 
+class ModerationValidator:
+	@staticmethod
+	def can_moderate_user(guild: discord.Guild, moderator: discord.member, target: discord.Member, bot: discord.Member) -> tuple[bool, str]:
+		if target.id == moderator.id:
+			return False, "can't do that to urself buddy"
+
+		if target.id == bot.id:
+			return False, "i am IMMUNE!!!"
+
+		if target.id == guild.owner_id:
+			return False, "hey dont do that to the owner :("
+
+		if moderator.id == guild.owner_id:
+			return True, ""
+
+		if moderator.top_role <= target.top_role:
+			return False, "your league is too low lol"
+
+		if bot.top_role <= target.top_role:
+			return False, "my league is too low lol"
+
+		return True, ""
+
+	@staticmethod
+	def has_permission_for_action(member: discord.Member, action: ModerationAction) -> bool:
+		permission_map = {
+			ModerationAction.BAN: member.guild_permissions.ban_members,
+			ModerationAction.UNBAN: member.guild_permissions.ban_members,
+			ModerationAction.KICK: member.guild_permissions.kick_members,
+			ModerationAction.TIMEOUT: member.guild_permissions.moderate_members,
+			ModerationAction.UNTIMEOUT: member.guild_permissions.moderate_members
+		}
+
+		return permission_map.get(
+			action,
+			False
+		)
